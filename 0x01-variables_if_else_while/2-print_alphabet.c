2a993be8a0bf7a81b52a2ee5e5a4b77a5b52eaa0
Maame3si <hannahoa@icloud.com>
2023-04-21 16:15:58 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int char - 'a';
+
+	while (char <- 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar{'\n'};
+	return (0);
+}
