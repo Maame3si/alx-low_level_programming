29552e3ac447b3b5002398c7f09b8305861041d8
Maame3si <hannahoa@icloud.com>
2023-04-22 03:39:57 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,31 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main -main function to generate a radom number
- *
- * Return:Always 0 (Sucess)
- *
+ *main - Entry point
+ *Description: 'get the last degit of a number'
+ *Return: Always 0
  */
 int main(void)
 {
         int n;
-        int num;
+        int ld;
 
         srand(time(0));
         n = rand() - RAND_MAX / 2;
-        printf("Last digit of %d is ", n);
-        num = n % 10;
-        if (num > 5)
+	ld = n % 10;
+	if (ld > 5)
+	{
+                printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+	}
+        else if (ld == 0)
         {
-                printf("%d and is greater than 5\n", num);
-        }
-        else if ((num < 6) && (num < 0))
-        {
-		printf("%d and is less than 6 and not 0\n", num);
+		printf("Last digit of %d is %d and is 0\n", n, ld);
         }
         else
-        {
-                printf("%d and is 0\n", num);
-        }
-        return (0);
+	{
+		printf("Last digit of %d is %d and less than 6 and not 0\n", n, ld);
+	}
+	return (0);
 }
