1bd6fb33b72aad11d044406e256dc26d84f88bb3
Maame3si <hannahoa@icloud.com>
2023-04-22 19:05:11 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 
 /** main - print "and that piece of art is useful" - Dora korpar, 2015-10-19",
   * followed by a newline, to standard error.
-  * Return : Always 1,
+  * Return: Always 1,
   */
 
 
