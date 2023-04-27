b37b4e8dd12c40e01a2d876dc6cf1e03d49a1960
Maame3si <hannahoa@icloud.com>
2023-04-27 03:28:53 -0700

    0-isupper.c

---
+++ 0-isupper.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _isuper - Check if a letter is supper
+ * @x: The number to be checked
+ * Return: 1 for upper letter or 0 for any else
+ */
+
+int _supper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}#include "main.h"
+
+/**
+ * _isuper - Check if a letter is supper
+ * @x: The number to be checked
+ * Return: 1 for upper letter or 0 for any else
+ */
+
+int _supper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}#include "main.h"
+
+/**
+ * _isuper - Check if a letter is supper
+ * @x: The number to be checked
+ * Return: 1 for upper letter or 0 for any else
+ */
+
+int _supper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+
+int maiin(void)
+{
+        print_to_98(0);
+        print_to_98(98);
+        rint_to_98(111);
+        rint_to_98(81);
+        rint_to_98(-10);
+        return (0);
+}
