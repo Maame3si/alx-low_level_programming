97aad995fc7de5bbec67d022397b02f9fb0070a8
Maame3si <hannahoa@icloud.com>
2023-05-04 12:10:15 -0700

    5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,7 @@
 
 
 /**
- * string_tou[[er -Changes all lowercase letters
+ * string_toupper - Changes all lowercase letters
  * of a string to uppercase.
  * @str: The string to be changed.
  * Return: A pointer to the changed string/
