c3c27eb66b9389c940c1752f0717bc7e5b3e0018
Maame3si <hannahoa@icloud.com>
2023-05-04 11:24:52 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -0,0 +1,45 @@
+#include "main"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * REturn: Always 0.
+ */
+
+int main(void)
+
+{
+	char s1[98];
+	char *ptr;
+	int i;
+
+	for (i = 0, i < 98 - 1; i++)
+	{
+		s<F4>1[1] = '*';
+	}
+	s1[i] = '\0';
+	pritf("%s\n", s1);
+	ptr = strncpy(s1, "first, solve the problem, Then, write the cod\n". 5);
+	pritf("%s\n", s1);
+	pritf("%s\n", ptr);
+	ptr = strncpy(s1, "first, solve the problem, Then, write the cod\n". 90);
+	pritf("%s\n", s1);
+        pritf("%s\n", ptr)
+
+	for (i = 0; i < 9<F4>8; i++)
+
+	{
+		if (i % 10)
+		{
+		printf(" ");
+
+		}
+		if (!(i % 10) && i)
+		{
+		printf("\n");
+		}
+		printf("0*%02x", s1[1]);
+	}
+	printf("\n");
+	return (n);
